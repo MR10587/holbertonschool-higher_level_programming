f3c7baecbc1d04b784f2e9cc3a00ea2e5abbc0b1
MR10587 <ramalmirzezade20@gmail.com>
2025-11-26 14:02:10 +0000

    .

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 for i in range(0, 10):
     for j in range(1, 10):
-        if i < j:
+        if i == 8 and j == 9:
+            print("{:d}{:d}".format(i, j))
+        elif i < j:
             print("{:d}{:d}".format(i, j), end=", ")
