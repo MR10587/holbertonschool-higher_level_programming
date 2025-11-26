c8b8d1abd4ca7e7615ead8007bbb2cdf6f0c92db
MR10587 <ramalmirzezade20@gmail.com>
2025-11-26 13:30:54 +0000

    .

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 for i in range(100):
-    if i >= 0 and i <= 9:
-        print(f"0{i}", end=", ")
-    elif i == 99:
-        print(i)
+    if i == 99:
+        print("{:02d}".format(i))
     else:
-        print(i, end=", ")
+        print("{:02d}".format(i), end=", ")
