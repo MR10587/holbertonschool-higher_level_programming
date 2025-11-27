8d7353510f7bfa40c1959ecc62aa6d98133f72d9
MR10587 <ramalmirzezade20@gmail.com>
2025-11-27 11:26:31 +0000

    .

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def uppercase(str):
-    for i in range(123):
-        if i >= 97 and i <= 122:
-            ord(str) == i - 32
-        else:
-            print(str)
+    for i in str:
+        if ord(i) >= 97 and ord(i) <= 122:
+            i = chr(ord(i) - 32)
+        print(i, end="")
