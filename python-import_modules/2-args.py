a170d1c09a42a73da2a1b1fdeab98ae8ca9d3c49
MR10587 <ramalmirzezade20@gmail.com>
2025-12-01 12:49:08 +0000

    .

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -3,7 +3,7 @@ import sys
 if __name__ == '__main__':
     if len(sys.argv) == 1:
         print(f'{len(sys.argv) - 1} arguments.')
-    elif len(sys.argv) > 1:
+    elif len(sys.argv) >= 1:
         print(f'{len(sys.argv) - 1} arguments:')
         for i in range(1, len(sys.argv)):
             print(f'{i}: {sys.argv[i]}')
