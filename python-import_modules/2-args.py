038e29daf56d439f7420814e5c8492a91b14200c
MR10587 <ramalmirzezade20@gmail.com>
2025-12-01 12:46:47 +0000

    .

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import sys
+if len(sys.argv) == 1:
+    print(f'{len(sys.argv) - 1} arguments.')
+elif len(sys.argv) > 1:
+    print(f'{len(sys.argv) - 1} arguments:')
+    for i in range(1, len(sys.argv)):
+        print(f'{i}: {sys.argv[i]}')
