be66c31050dd6e3ebd2b0da2f285abf291926710
MR10587 <ramalmirzezade20@gmail.com>
2025-12-01 13:27:15 +0000

    .

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 import sys
 
+sum = 0
 if __name__ == "__main__":
     for i in range (1, len(sys.argv)):
-        print(sys.argv[i])
-        i+= i
-        print(i)
+        sum += int(sys.argv[i])
+    print(sum)
