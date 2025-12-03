ae5cff4408527af1f8e31aa4f9901e3cddc8edaf
MR10587 <ramalmirzezade20@gmail.com>
2025-12-03 11:18:40 +0000

    .

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
     for i in my_list:
-        while type(my_list[i]) == str:
-            if type(i) is int:
-                print('{:d}'.format(i))
+        if type(i) is int:
+            print('{:d}'.format(i))
