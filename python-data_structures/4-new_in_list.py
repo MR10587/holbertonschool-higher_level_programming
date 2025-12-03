e20eef413cfc245d9d00bac5938034384db57a0a
MR10587 <ramalmirzezade20@gmail.com>
2025-12-03 19:15:34 +0000

    .

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -3,6 +3,6 @@ def new_in_list(my_list, idx, new_element):
     if idx < 0 or idx >= len(my_list):
         return my_list
     else:
-        my_list[idx] = new_element
-        new_list = my_list
+        new_list = my_list.copy()
+        new_list[idx] = new_element
         return new_list
