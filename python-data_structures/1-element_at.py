7e57a1319185bb2612f57e60488c35b0cb49a15a
MR10587 <ramalmirzezade20@gmail.com>
2025-12-03 18:11:03 +0000

    .

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -2,5 +2,5 @@
 def element_at(my_list, idx):
     if idx == 0 or idx >= len(my_list):
         return None
-    elif idx >= 0 and ids < len(my_list):
+    elif idx >= 0 and idx < len(my_list):
         print("Element at index {:d} is {}".format(idx, element_at(my_list, idx)))
