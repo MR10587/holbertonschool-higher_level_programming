d93b0f5d2b2b604263aed3a9261e5d1be94b02f9
MR10587 <ramalmirzezade20@gmail.com>
2025-12-04 18:27:46 +0000

    .

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
+sentence = ''
 def multiple_returns(sentence):
     length = len(sentence)
     if length != 0:
         first = sentence[0]
     else:
-        return None
+        first = None
     new_tuple = tuple([length, first])
     return new_tuple
