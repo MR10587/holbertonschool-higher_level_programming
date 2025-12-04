8c9f13838fce09edc9e31e6b09b47cba34d8bff5
MR10587 <ramalmirzezade20@gmail.com>
2025-12-04 18:08:11 +0000

    .

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -12,3 +12,5 @@ def add_tuple(tuple_a=(), tuple_b=()):
         tuple_b = tuple(tuple_b[0], 0)
     elif len(tuple_b) == 0:
         tuple_b = tuple(0, 0)
+    new_tuple = tuple(tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+    return new_tuple
