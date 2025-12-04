67138c674744f5e0bdc37e0935b752ac2988c8a6
MR10587 <ramalmirzezade20@gmail.com>
2025-12-04 15:15:44 +0000

    .

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,16 +1,8 @@
 #!/usr/bin/python3
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
 def print_matrix_integer(matrix=[[]]):
     for i in matrix:
         for index, j in enumerate(i):
             if index != 0:
-                print(' ', end='')  # print space before elements except first
+                print(' ', end='')
             print('{:d}'.format(j), end='')
-        print()  # newline after each row
-
-        
- 
+        print()
