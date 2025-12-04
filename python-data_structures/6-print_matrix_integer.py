39f7847d1c19e7350757673628656f73d145e885
MR10587 <ramalmirzezade20@gmail.com>
2025-12-04 15:03:53 +0000

    .

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,4 +1,4 @@
-#!/usr//bin/python3
+#!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     print('{}, {}, {}'.format(matrix[0][0], matrix[0][1], matrix[0][2]))
     print('{}, {}, {}'.format(matrix[1][0], matrix[1][1], matrix[1][2]))
