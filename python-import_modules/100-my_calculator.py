5b127a98589115490ceefa97b14bf13f5129f997
MR10587 <ramalmirzezade20@gmail.com>
2025-12-05 08:55:42 +0000

    .

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-import sys
+from sys import argv
 from calculator_1 import add, sub, mul, div
 
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
+    if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
     else:
-        operator = sys.argv[2]
-        a: int = int(sys.argv[1])
-        b: int = int(sys.argv[3])
+        operator = argv[2]
+        a: int = int(argv[1])
+        b: int = int(argv[3])
         if operator == '+':
             result = add(a, b)
             print('{:d} {} {:d} = {:d}'.format(a, operator, b, result))
