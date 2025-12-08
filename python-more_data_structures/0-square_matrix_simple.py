50b4be142352c189930e2de330eeaf46f993bc89
MR10587 <ramalmirzezade20@gmail.com>
2025-12-08 11:16:19 +0000

    .

---
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    new_matrix = []
+    for i in range(len(matrix)):
+        arr = []
+        for j in matrix[i]:
+            j = j**2
+            arr.append(j)
+        new_matrix.append(arr)
