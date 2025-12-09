883d1732464fbed15b6391ed5ce2b0e80d415a20
MR10587 <ramalmirzezade20@gmail.com>
2025-12-09 07:37:30 +0000

    .

---
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+    a = []
+    for i in a_dictionary:
+        a.append(f'{i}: {a_dictionary[i]}')
+    b = sorted(a)
+    for i in b:
+        return i
