13d3e9286e319ac11f4cf46015fc18f048e4f3fc
MR10587 <ramalmirzezade20@gmail.com>
2025-12-10 06:22:17 +0000

    .

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    # dictionary of character as key and values
+    roman_num = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000, 'IV': 4, 'IX': 9, 'XL': 40, 'XC': 90, 'CD': 400, 'CM': 900}
+    # add values to total
+    character, total = 0, 0
+    while character < len(roman_string):
+        if character + 1 < len(roman_string) and roman_string[character:character + 2] in roman_num:
+            total += roman_num[roman_string[character:character + 2]]
+            character +=2
+        else:
+            total += roman_num[roman_string[character]]
+            character +=1
+        
+    # return total
+    return total
