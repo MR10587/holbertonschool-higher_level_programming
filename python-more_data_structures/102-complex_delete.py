1933a9804ab61eaeedba9c02f27ad48bbf2306a5
MR10587 <ramalmirzezade20@gmail.com>
2025-12-10 17:28:09 +0000

    .

--- python-more_data_structures/102-complex_delete.py
+++ python-more_data_structures/102-complex_delete.py
@@ -4,4 +4,5 @@ def complex_delete(a_dictionary, value):
     for i in a_dictionary:
         if a_dictionary[i] != value:
             b_dictionary[i] = a_dictionary[i]
+    a_dictionary = b_dictionary
     return b_dictionary
