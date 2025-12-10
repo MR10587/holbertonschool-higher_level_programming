639e8165f4fb83ffb47558a75b0646bc172f4e0a
MR10587 <ramalmirzezade20@gmail.com>
2025-12-10 17:44:08 +0000

    .

--- python-more_data_structures/102-complex_delete.py
+++ python-more_data_structures/102-complex_delete.py
@@ -4,4 +4,3 @@ def complex_delete(a_dictionary, value):
         if a_dictionary[i] == value:
             del a_dictionary[i]
     return a_dictionary
-print(complex_delete({'lang': "C", 'track': "Low", 'pref': "C", 'ids': [1, 2, 3]}, 'C'))
