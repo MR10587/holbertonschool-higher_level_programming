e4695545b5136b3a03b2bb17e22f265542177482
MR10587 <ramalmirzezade20@gmail.com>
2025-12-12 08:42:52 +0000

    .

---
+++ python-exceptions/3-safe_print_division.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    try:
+        result = float(a / b)
+    except (ZeroDivisionError, TypeError, ValueError):
+        result = None
+    finally:
+        print('Inside result: {}'.format(result))
