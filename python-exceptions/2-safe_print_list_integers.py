3197c18dea5a6526e0e5d7c5a2584107c9b188c5
MR10587 <ramalmirzezade20@gmail.com>
2025-12-12 08:31:10 +0000

    .

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    integer = 0
+    for i in my_list:
+        try:
+            print("{:d}".format(i), end='')
+            integer += 1
+        except (ValueError, TypeError):
+            continue
+    print()
+    return integer
