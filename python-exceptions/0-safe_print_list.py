62de8c34a11bbd972c362518f860516197c86f87
MR10587 <ramalmirzezade20@gmail.com>
2025-12-12 06:53:45 +0000

    .

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    for i in range(0, x):
-        try:
-            print(my_list[i], end='')
-        except:
-            print()
-            break
-    print()
-    return i + 1
-    print()
+    if x > 0:
+        for i in range(0, x):
+            try:
+                print(my_list[i], end='')
+            except:
+                print()
+                break
+        print()
+        return i + 1
+        print()
+    else:
+        return 0
