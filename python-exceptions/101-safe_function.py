28b23253f43ff09ea53f4d6563f4e2e391de8c12
MR10587 <ramalmirzezade20@gmail.com>
2025-12-13 06:29:06 +0000

    .

--- python-exceptions/101-safe_function.py
+++ python-exceptions/101-safe_function.py
@@ -4,7 +4,7 @@ import sys
 
 def safe_function(fct, *args):
     try:
-        return my_div(a, b)
+        return fct
     except ZeroDivisionError as a:
         print('Exception: {}'.format(a), file=sys.stderr)
         return None
