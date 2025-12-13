06bf361f67ba47a5a311333c679ba18cf033378f
MR10587 <ramalmirzezade20@gmail.com>
2025-12-13 06:44:35 +0000

    .

---
+++ python-exceptions/102-magic_calculation.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def magic_calculation(a, b):
+    for i in range(1, 3):
+        try:
+            if i > a:
+                print('Exception: Too far')
+            result += a**b / i
+        except TypeError, ValueError:
+            result = a + b
+            break
+    return result
