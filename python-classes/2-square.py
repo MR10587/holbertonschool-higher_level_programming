35965f7bdfa48adf542570a7921e399001c90c64
MR10587 <ramalmirzezade20@gmail.com>
2025-12-16 08:16:20 +0000

    .

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -4,7 +4,7 @@
 
 class Square:
     """python3 -c 'print(__import__("my_module").MyClass.__doc__)"""
-    def __init__(self, size):
+    def __init__(self, size=0):
         """python3 -c 'print(__import__("my_module").my_function.__doc__)'
         python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'
         """
