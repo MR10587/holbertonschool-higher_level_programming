0d272a20ed557f1b682359f25b91e1d251eb68a7
MR10587 <ramalmirzezade20@gmail.com>
2025-12-17 18:30:54 +0000

    .

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -32,7 +32,7 @@ class Square:
 
     def my_print(self):
         if self.__size != 0:
-            for i in range(self._size):
+            for i in range(self.__size):
                 print('#' * self.__size)
         else:
             print('')
