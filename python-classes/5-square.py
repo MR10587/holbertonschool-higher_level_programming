941c689644038b31f184180f9b25524275ef0cfd
MR10587 <ramalmirzezade20@gmail.com>
2025-12-17 18:27:32 +0000

    .

---
+++ python-classes/5-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+
+class Square:
+    """python3 -c 'print(__import__("my_module").MyClass.__doc__)"""
+    def __init__(self, size=0):
+        """python3 -c 'print(__import__("my_module").my_function.__doc__)'
+        python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'
+        """
+        self.__size = size
+        if type(self.__size) is not int:
+            raise TypeError('size must be an integer')
+        elif self.__size < 0:
+            raise ValueError('size must be >= 0')
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        self.__size = value
+        if type(value) is not int:
+            raise TypeError('size must be an integer')
+        elif value < 0:
+            raise ValueError('size must be >= 0')
+
+    def area(self):
+        self.__area = self.__size ** 2
+        return self.__area
+
+    def my_print(self):
+        if self.__size != 0:
+            print('#' * self.__size)
+        else:
+            print('')
