9549636539eb0954c2523283d7299073cd42f228
RM10587 <ramalmirzezade20@gmail.com>
2025-12-19 18:01:04 +0000

    .

---
+++ python-more_classes/2-rectangle.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""python3 -c 'print(__import__("my_module").__doc__)"""
+
+
+class Rectangle:
+    """python3 -c 'print(__import__("my_module").MyClass.__doc__)"""
+    def __init__(self, width=0, height=0):
+        """python3 -c 'print(__import__("my_module").my_function.__doc__)'
+        python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError('width must be an integer')
+        if value < 0:
+            raise ValueError('width must be >= 0')
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError('height must be an integer')
+        if value < 0:
+            raise ValueError('height must be >= 0')
+        self.__height = value
+
+    def area(self):
+        return self.__height * self.__width
+
+    def perimeter(self):
+        return self.__width + self.__height
