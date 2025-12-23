0f1574c29f40b9469be63c3928c83f18cb4f18cb
MR10587 <ramalmirzezade20@gmail.com>
2025-12-23 18:44:45 +0000

    .

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -2,10 +2,10 @@
 """python3 -c 'print(__import__("my_module").__doc__)'"""
 
 
-def is_kind_of_class(obj, a_class):
+def inherits_from(obj, a_class):
     '''python3 -c 'print(__import__("my_module").my_function.__doc__)'
     python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'
     '''
-    if issubclass(type(obj).__name__, a_class):
+    if issubclass(obj.__class__, a_class):
         return True
     return False
