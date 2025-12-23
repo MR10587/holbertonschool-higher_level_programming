762ea57264d40b63a8d06489a358d966409c2d05
MR10587 <ramalmirzezade20@gmail.com>
2025-12-23 18:23:27 +0000

    .

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -6,6 +6,6 @@ def is_same_class(obj, a_class):
     '''python3 -c 'print(__import__("my_module").my_function.__doc__)'
     python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'
     '''
-    if type(obj) == a_class:
+    if type(obj) is a_class:
         return True
     return False
