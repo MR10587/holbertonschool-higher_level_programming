47bd2aad672eb069a02d1281beadd37d7e776664
MR10587 <ramalmirzezade20@gmail.com>
2025-12-23 06:11:23 +0000

    .

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-'''python3 -c 'print(__import__("my_module").__doc__)''''
+"""python3 -c 'print(__import__("my_module").__doc__)'"""
 
 
 def lookup(obj):
-    '''
+    """
     python3 -c 'print(__import__("my_module").my_function.__doc__)'
     python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'
-    '''
+    """
     return [dir(obj)]
