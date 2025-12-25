c3960918be317554d34dbccdac319e2c38719cfd
MR10587 <ramalmirzezade20@gmail.com>
2025-12-25 07:45:12 +0000

    .

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,7 +1,19 @@
 #!/usr/bin/python3
 """asda"""
 
-from 7-base_geometry import BaseGeometry
+class BaseGeometry:
+    """sadasdas"""
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if type(value) is int:
+            if value > 0:
+                pass
+            else:
+                raise ValueError("{} must be greater than 0".format(name))
+        else:
+            raise TypeError("{} must be an integer".format(name))
 
 
 class Rectangle(BaseGeometry):
