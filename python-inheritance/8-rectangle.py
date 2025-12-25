e1438e614b10622e7e064090dd1b9376e69fe99e
MR10587 <ramalmirzezade20@gmail.com>
2025-12-25 07:43:38 +0000

    .

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""asda"""
+
+from 7-base_geometry import BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """sadasdas"""
+
+    def __init__(self, width, height):
+        self.__width = integer_validator(self, name, width)
+        self.__height = integer_validator(self, name, height)
