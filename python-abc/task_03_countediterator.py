fb60f543a06f60718cf0490d796d5b1749ac20db
MR10587 <ramalmirzezade20@gmail.com>
2025-12-27 17:28:04 +0000

    .

---
+++ python-abc/task_03_countediterator.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+class CountedIterator:
+
+    def __init__(self, iterator, counter=0):
+        self.iterator = iter(iterator)
+        self.counter = counter
+
+    def get_count(self):
+        return self.counter
+
+    def __next__(self):
+        self.counter += 1
+        item = next(self.iterator)
+        if item in data:
+            return item
+        raise StopIteration
+
+
+data = [4, 3, 2, 1]
+counted_iter = CountedIterator(data)
+
+try:
+    while True:
+        item = next(counted_iter)
+        print(f"Got {item}, total {counted_iter.get_count()} items iterated.")
+except StopIteration:
+    print("No more items.")
