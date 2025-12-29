761a7ac716e7166d951ed5394a9bac9b84e69d59
MR10587 <ramalmirzezade20@gmail.com>
2025-12-29 18:46:02 +0000

    ;..

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -6,8 +6,4 @@ def write_file(filename="", text=""):
     """Writing Text"""
     with open(filename, 'w', encoding='utf-8') as f:
         f.write(text)
-
-    total = 0
-    for char in filename:
-        total += 1
-    return total
+        f.tell()
