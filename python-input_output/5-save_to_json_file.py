a9e00488b54edac1163e6b872370ab4ba5e29df9
MR10587 <ramalmirzezade20@gmail.com>
2025-12-30 16:34:57 +0000

    ..

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -7,6 +7,6 @@ import json
 
 def save_to_json_file(my_obj, filename):
     """Sagol"""
-    with open(filename, 'w', 'utf-8') as f:
+    with open(filename, 'w', encoding='utf-8') as f:
         f.write(json.dumps(my_obj))
         return f
