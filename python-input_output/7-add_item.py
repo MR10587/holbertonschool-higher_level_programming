0a0f0f5cf20cdfa37d7bbf9ff890906e6849ae47
MR10587 <ramalmirzezade20@gmail.com>
2025-12-30 17:11:10 +0000

    .

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -7,10 +7,7 @@ import json
 save = __import__('5-save_to_json_file').save_to_json_file
 load = __import__('6-load_from_json_file').load_from_json_file
 
-
-load('add_item.json')
-
-text = []
+text = load('add_item.json')
 for arg in sys.argv[1:]:
     text.append(arg)
 
