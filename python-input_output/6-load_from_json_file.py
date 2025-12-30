be6fb9561e5206b88d47242a3ce65fe2505ff011
MR10587 <ramalmirzezade20@gmail.com>
2025-12-30 16:38:50 +0000

    .

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Salam"""
+
+
+import json
+
+
+def load_from_json_file(filename):
+    """Sagol"""
+    with open(filename, 'w', encoding='utf-8') as f:
+        return json.loads(filename)
