68ad610ee927fbfa59ae146b272fa88c5ae7b2d1
MR10587 <ramalmirzezade20@gmail.com>
2025-12-30 17:07:25 +0000

    .

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Salam"""
+import sys
+import json
+
+
+save = __import__('5-save_to_json_file').save_to_json_file
+load = __import__('6-load_from_json_file').load_from_json_file
+
+
+load('add_item.json')
+
+text = []
+for arg in sys.argv[1:]:
+    text.append(arg)
+
+save(text, 'add_item.json')
