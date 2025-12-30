1a79f9950ea2a9af657aa7d6cb34faa4a87517c3
MR10587 <ramalmirzezade20@gmail.com>
2025-12-30 17:18:56 +0000

    .

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
-"""Salam"""
+"""
+Adds all command-line arguments to a Python list
+and saves it to a JSON file.
+"""
 import sys
-
 
 save = __import__('5-save_to_json_file').save_to_json_file
 load = __import__('6-load_from_json_file').load_from_json_file
 
-text = load('add_item.json')
+items = load('add_item.json')
 
 for arg in sys.argv[1:]:
-    text.append(arg)
+    items.append(arg)
 
-save(text, 'add_item.json')
+save(items, 'add_item.json')
+
