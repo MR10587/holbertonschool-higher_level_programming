1e135df27f3b5045054b1f2d7d27526e5f44ff80
MR10587 <ramalmirzezade20@gmail.com>
2026-01-01 10:33:18 +0000

    .

---
+++ python-input_output/11-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Salam"""
+
+
+class Student:
+    """Students"""
+    def __init__(self, first_name, last_name, age):
+        """Student Data"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """to json"""
+        if isinstance(attrs, list):
+            final = {}
+            for i in attrs:
+                if isinstance(i, str) and hasattr(self, i):
+                    final[i] = getattr(self, i)
+            return final
+        return vars(self)
+
+    def reload_from_json(self, json):
+        json[first_name] = first_name
+        json[last_name] = last_name
+        json[age] = age
