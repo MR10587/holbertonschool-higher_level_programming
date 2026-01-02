8f03dbb37e2f380df0bcaca2304ecc93c3de473b
MR10587 <ramalmirzezade20@gmail.com>
2026-01-02 18:09:19 +0000

    pycod

---
+++ task_00_basic_serialization.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import json
+
+
+def serialize_and_save_to_file(data, filename):
+    # Your code here to serialize and save data to the specified file
+    with open(filename, 'w') as f:
+        return f.write(json.dump(data))
+
+
+def load_and_deserialize(filename):
+    # Your code here to load and deserialize data from the specified file
+    with open(filename, 'r') as f:
+        return json.load(f)
