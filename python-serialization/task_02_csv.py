6592db862b5a58b11047b2ae2a7be03915e5f219
MR10587 <ramalmirzezade20@gmail.com>
2026-01-04 09:29:57 +0000

    .

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -5,11 +5,11 @@ import json
 
 def convert_csv_to_json(filename):
     try:
-        with open('filename.csv', 'r', newline='' ) as f:
+        with open(filename, 'r', newline='' ) as f:
             data = csv.DictReader(f)
-        with open('data.json', 'w') as a:
-            for i in data:
-                json.dump(i, a)
+            with open(data, 'w') as a:
+                for i in data:
+                    json.dump(i, a)
         return True
     except FileNotFoundError:
         return False
