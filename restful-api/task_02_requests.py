079e61440cf37ac67a708bee02bc46faa9d099db
MR10587 <ramalmirzezade20@gmail.com>
2026-01-07 14:12:29 +0000

    .

---
+++ restful-api/task_02_requests.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+import requests
+import csv
+
+
+url = 'https://jsonplaceholder.typicode.com/posts'
+
+
+def fetch_and_print_posts():
+    a = requests.get(url)
+    print(f"Status code: {a.status_code}")
+
+    if a.status_code == 200:
+        j = a.json()
+        for title in j:
+            print(title["title"])
+
+
+def fetch_and_save_posts():
+    a = requests.get(url)
+
+    if a.status_code == 200:
+        j = a.json()
+        new_post = []
+        for post in j:
+            new_post.append({
+                "id": post["id"],
+                "title": post["title"],
+                "body": post["body"]
+            })
+        with open(posts.csv, 'w', newline='', encoding='utf-8') as f:
+            new_file = csv.DictWriter(
+                f,
+                fieldnames=["id", "title", "body"]
+            )
+            new_file.writeheader()
+            new_file.writerows(new_post)
