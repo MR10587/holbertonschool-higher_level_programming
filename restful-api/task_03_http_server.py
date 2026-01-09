03370f2e89cb86d61b1416e900669280d56298ba
MR10587 <ramalmirzezade20@gmail.com>
2026-01-09 19:09:32 +0000

    .

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -1,18 +1,40 @@
 #!/usr/bin/python3
 import http.server
+import socketserver
 
 
-class handler(http.server.BaseHTTPRequestHandler):
+dataset = {"name": "John", "age": 30, "city": "New York"}
+to_json = json.dumps(dataset)
+
+class Handler(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-Type", "text/plain; charset=utf-8")
-        self.end_headers()
-        self.wfile.write(b'Hello, this is a simple API!')
+        if self.path == '/':
+            self.send_response(200)
+            self.send_header("Content-Type", "text/plain")
+            self.end_headers()
+            
+            self.wfile.write(b'Hello, this is a simple API!')
+        elif self.path == '/data':
+            self.send_response(200)
+            self.send_header("Content-Type", "application/json")
+            self.end_headers()
+            
+            self.wfile.write(to_json.encode())
+        
+        elif self.path == '/status':
+            self.send_response(200)
+            self.send_header("Content-Type", "text/plain")
+            self.end_headers()
+            
+            self.wfile.write(b'OK')
+        
+        else:
+            self.send_response(404)
+            self.send_header("Content-Type", "text/plain")
+            self.end_headers()
 
-def run_server():
-    server_address = ('', 8080)
-    httpd = http.server.HTTPServer(server_address, handler)
-    httpd.serve_forever()
+            self.wfile.write(b'Endpoint not found')
+        
 
-if __name__ == "__main__":
-    run_server()
+httpd = socketserver.TCPServer(("", 9990), Handler)
+httpd.serve_forever()
