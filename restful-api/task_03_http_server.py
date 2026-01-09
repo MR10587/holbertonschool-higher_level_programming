f2c8fe4673c125b85271ed6bd5f2687383c40c67
MR10587 <ramalmirzezade20@gmail.com>
2026-01-09 19:12:07 +0000

    .

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -6,15 +6,13 @@ dictionary = {"name": "John", "age": 30, "city": "New York"}
 json_sample = json.dumps(dictionary)
 
 
-sub = http.server.SimpleHTTPRequestHandler
-
-class Handler(sub):
+class Handler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
         if self.path == '/':
             self.send_response(200)
             self.send_header("Content-type", 'text/plain')
             self.end_headers()
-            
+
             self.wfile.write(b'Hello, this is a simple API!')
         elif self.path == '/data':
             self.send_response(200)
