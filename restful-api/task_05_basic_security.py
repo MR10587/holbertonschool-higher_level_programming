46852e968565bd681c841da2100a0bf897c1d8cb
MR10587 <ramalmirzezade20@gmail.com>
2026-01-11 18:31:15 +0000

    .

--- restful-api/task_05_basic_security.py
+++ restful-api/task_05_basic_security.py
@@ -16,6 +16,8 @@ auth = HTTPBasicAuth()
 
 app.config["JWT_SECRET_KEY"] = "secret_key"
 
+jwt = JWTManager(app)
+
 users = {
     "user1": {"username": "user1", "password": generate_password_hash("password"), "role": "user"},
     "admin1": {"username": "admin1", "password": generate_password_hash("password"), "role": "admin"}
