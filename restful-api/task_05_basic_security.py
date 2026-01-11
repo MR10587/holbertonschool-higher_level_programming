57d6849113687b3e9984a4e6cb0c78929c112b82
MR10587 <ramalmirzezade20@gmail.com>
2026-01-11 19:13:05 +0000

    .

--- restful-api/task_05_basic_security.py
+++ restful-api/task_05_basic_security.py
@@ -1,22 +1,17 @@
 #!/usr/bin/python3
-from flask import Flask
-from flask import jsonify
-from flask import request
+from flask import Flask, jsonify, request
 from flask_httpauth import HTTPBasicAuth
 from werkzeug.security import generate_password_hash, check_password_hash
-
-from flask_jwt_extended import create_access_token
-from flask_jwt_extended import get_jwt_identity
-from flask_jwt_extended import jwt_required
-from flask_jwt_extended import JWTManager
-
+from flask_jwt_extended import (
+    JWTManager, create_access_token, get_jwt_identity, jwt_required
+)
 
 app = Flask(__name__)
 auth = HTTPBasicAuth()
 
 app.config["JWT_SECRET_KEY"] = "secret_key"
-
 jwt = JWTManager(app)
+
 
 users = {
     "user1": {"username": "user1", "password": generate_password_hash("password"), "role": "user"},
@@ -30,25 +25,38 @@ def verify_password(username, password):
 
 @app.route('/basic-protected', methods=['GET'])
 @auth.login_required
-def index():
+def basic_protected():
     return "Basic Auth: Access Granted"
+
+@app.route('/user', methods=['GET'])
+@auth.login_required(role='user')
+def user_entry():
+    return f"Hello {auth.current_user()}, you are a user"
 
 @app.route('/login', methods=['POST'])
 def login():
     username = request.json.get("username", None)
     password = request.json.get("password", None)
-    
+
     user = users.get(username)
     if not user or not check_password_hash(user['password'], password):
         return jsonify({"msg": "Wrong username or password"}), 401
-    
+
     access_token = create_access_token(identity=username)
     return jsonify(access_token=access_token)
 
 @app.route('/jwt-protected', methods=['GET'])
 @jwt_required()
-def protected():
+def jwt_protected():
     return "JWT Auth: Access Granted", 200
+
+@app.route('/admin', methods=['GET'])
+@jwt_required()
+def admin():
+    identity = get_jwt_identity()
+    if users[identity]["role"] != "admin":
+        return "Forbidden", 403
+    return "Admin Access Granted", 200
 
 @jwt.invalid_token_loader
 def invalid_token_callback(error):
@@ -62,23 +70,6 @@ def missing_token_callback(error):
 def expired_token_callback(jwt_header, jwt_payload):
     return "Token expired", 401
 
-@auth.get_user_roles
-def get_user_roles(username):
-    return users[username]["role"]
-
-@app.route('/admin', methods=['GET'])
-@jwt_required()
-def admin():
-    identity = get_jwt_identity()
-    if users[identity]["role"] != "admin":
-        return "Forbidden", 403
-    return "Admin Access Granted", 200
-
-
-@app.route('/user')
-@auth.login_required(role='user')
-def user_entry():
-    return f"Hello {auth.current_user()}, you are a user"
 
 if __name__ == "__main__":
     app.run()
