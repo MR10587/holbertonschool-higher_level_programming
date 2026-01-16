f77869610c57d9f682a121d3026d0a3b57682520
MR10587 <ramalmirzezade20@gmail.com>
2026-01-16 08:51:21 +0000

    .

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,11 +1,12 @@
 -- FULL CREATION
-CREATE TABLE second_table(
-    id INT,
+CREATE TABLE IF NOT EXISTS second_table(
+    id INT PRIMARY KEY,
     name VARCHAR(256),
     score INT
-)
+);
 INSERT INTO second_table(id, name, score)
-VALUES(1, 'John', 10,
-2, 'Alex', 3,
-3, 'Bob', 14,
-4, 'George', 8);
+VALUES
+    (1, 'John', 10),
+    (2, 'Alex', 3),
+    (3, 'Bob', 14),
+    (4, 'George', 8);
