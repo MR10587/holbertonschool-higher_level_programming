41bbaa163864208351d5159276ff84e367fe48e2
MR10587 <ramalmirzezade20@gmail.com>
2026-01-18 12:08:54 +0000

    .

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -1,5 +1,5 @@
 -- MOVE TO 
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-SELECT CONVERT(name USING utf8mb4) FROM first_table;
+ALTER TABLE hbtn_0c_0.first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+SELECT CONVERT(name USING utf8mb4) FROM hbtn_0c_0.first_table;
 
