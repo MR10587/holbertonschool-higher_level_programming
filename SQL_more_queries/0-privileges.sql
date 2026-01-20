8c9038a8352daa830aa9f3d09a88587e496af2da
MR10587 <ramalmirzezade20@gmail.com>
2026-01-20 18:37:14 +0000

    .

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,5 @@
 -- GRANT ACCESS
+CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'password';
+CREATE USER 'user_0d_2'@'localhost' IDENTIFIED BY 'password';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost' WITH GRANT OPTION;
