14c4120b40b850cf27d1c8c824916497dfe65bf7
MR10587 <ramalmirzezade20@gmail.com>
2026-01-20 18:39:21 +0000

    .

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,5 +1,3 @@
 -- GRANT ACCESS
-CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'password';
-CREATE USER 'user_0d_2'@'localhost' IDENTIFIED BY 'password';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost' WITH GRANT OPTION;
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
