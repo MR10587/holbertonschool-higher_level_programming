d6c0bff9478f491596928e6a6f1e535ae2251c33
MR10587 <ramalmirzezade20@gmail.com>
2026-01-21 13:24:56 +0000

    .

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,4 @@
 --CREATE READ USER
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-USE hbtn_0d_2;
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+CREATE USER IF NOT EXISTS 'user_0d_2'@'host' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'host';
