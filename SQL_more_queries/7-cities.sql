8f02898e4a59acaa358d4b58ede7a33a584bcfb7
MR10587 <ramalmirzezade20@gmail.com>
2026-01-21 17:18:52 +0000

    .

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,10 +1,10 @@
 -- CITIES
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS cities(
+CREATE TABLE IF NOT EXISTS cities (
     id INT NOT NULL AUTO_INCREMENT,
     state_id INT NOT NULL,
     name VARCHAR(256) NOT NULL,
-    PRIMARY KEY(id),
-    FOREIGN KEY(state_id) REFERENCES states(id),
+    PRIMARY KEY (id),
+    FOREIGN KEY (state_id) REFERENCES states(id)
 );
