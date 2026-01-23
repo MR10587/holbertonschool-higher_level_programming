859c0648518aa5b902cb056bbe29098c97586922
MR10587 <ramalmirzezade20@gmail.com>
2026-01-23 18:26:45 +0000

    .

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,8 +1,7 @@
 -- MY GENRES
 SELECT tv_genres.name
 FROM tv_genres
-JOIN tv_show_genres
-ON tv_show_genres.genre_id = tv_genres.id
-JOIN tv_shows ON tv_shows.id = tv_genres.id
+JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id
+JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
 WHERE tv_show_genres.show_id = 8
 ORDER BY tv_genres.name;
