fff169f07c7f1eee31135202a1283f9d43ea89ca
MR10587 <ramalmirzezade20@gmail.com>
2026-01-23 18:19:05 +0000

    .

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,6 +1,8 @@
 -- MY GENRES
 SELECT tv_genres.name
 FROM tv_genres
-INNER JOIN tv_shows
-ON tv_shows.name = 'Dexter' 
+JOIN tv_show_genres
+ON tv_show_genres.genre_id = tv_genres.id
+JOIN tv_shows ON tv_shows.id = tv_genres.id
+WHERE tv_show_genres.show_id = 8
 ORDER BY tv_genres.name;
