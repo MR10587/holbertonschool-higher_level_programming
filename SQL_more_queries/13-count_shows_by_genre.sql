57955fbd1a6773aa6e693093718a431f48c7b082
MR10587 <ramalmirzezade20@gmail.com>
2026-01-23 07:16:38 +0000

    .

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -3,5 +3,6 @@ SELECT g.name AS genre, COUNT(s.genre_id) AS number_of_shows
 FROM tv_genres g
 LEFT JOIN tv_show_genres s
 ON g.id = s.genre_id
+WHERE g.name IS NOT NULL
 GROUP BY g.id, g.name
 ORDER BY number_of_shows DESC;
