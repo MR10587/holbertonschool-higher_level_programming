c8e11c5670cb7d53971ff2fbfa9c166bc498b248
MR10587 <ramalmirzezade20@gmail.com>
2026-01-25 17:19:50 +0000

    .

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""third ORM"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost", user=sys.argv[1],
+        password=sys.argv[2], database=sys.argv[3], port=3306
+        )
+
+    cs = db.cursor()
+    cs.execute(
+        "SELECT * FROM states WHERE BINARY name = %s ORDER BY id ASC",
+        (sys.argv[4],)
+    )
+    rows = cs.fetchall()
+    for row in rows:
+        print(row)
+
+    cs.close()
+    db.close()
