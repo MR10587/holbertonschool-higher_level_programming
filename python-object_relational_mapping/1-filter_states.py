40508ae40a5be921927ca40326542129fb458836
MR10587 <ramalmirzezade20@gmail.com>
2026-01-25 15:50:45 +0000

    .

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Second ORM"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost", user=sys.argv[1],
+        password=sys.argv[2], database=sys.argv[3], port=3306
+        )
+
+    cs = db.cursor()
+    cs.execute("SELECT * FROM states WHERE name LIKE %N ORDER BY id ASC")
+    rows = cs.fetchall()
+    for row in rows:
+        print(row)
+
+    cs.close()
+    db.close()
+
+
