4b2c1cd2e93605cedd00bc468db2b970a8944347
MR10587 <ramalmirzezade20@gmail.com>
2026-01-25 17:46:47 +0000

    .

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""third ORM"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host='localhost', user=sys.argv[1],
+        password=sys.argv[2], database=sys.argv[3], port=3306
+        )
+    cs = db.cursor
+    cs.execute(
+        "SELECT * FROM cities ORDER BY id ASC"
+    )
+    rows = cs.fetchall
+    for row in rows:
+        print(row)
+
+    cs.close()
+    db.close()
