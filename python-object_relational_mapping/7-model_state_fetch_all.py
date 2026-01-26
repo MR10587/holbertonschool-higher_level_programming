84fe0895043ede75cef2557390f053d50bfa01d1
MR10587 <ramalmirzezade20@gmail.com>
2026-01-26 07:53:03 +0000

    .

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
 """First No sql"""
 
-from sqlalchemy import Integer, Column, String
-from sqlalchemy.ext.declarative import declarative_base
+import sys
+from sqlalchemy import create_engine, text
 from model_state import Base, State
 
 if __name__ == "__main__":
     engine = create_engine(
         "mysql+mysqldb://%s:%s@localhost/%s"
-        %(sys.argv[1], sys.argv[2], sys.argv[3])
+        % (sys.argv[1], sys.argv[2], sys.argv[3])
     )
 
     with engine.connect() as conn:
         result = conn.execute(
-            "SELECT id, name FROM states ORDER BY id ASC"
+            text("SELECT id, name FROM states ORDER BY id ASC")
         )
 
         for row in result:
-            print(f"{row[0]}: {row[1]}")
+            print(f"{row.id}: {row.name}")
