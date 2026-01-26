197674a99fbb49315e2d5c9aa2f4b4fba94b7750
MR10587 <ramalmirzezade20@gmail.com>
2026-01-26 07:00:04 +0000

    .

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
+"""First No sql"""
 
 from sqlalchemy import Integer, Column, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class State(Base):
+    """Class"""
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False)
-    name = Colums(String(128), nullable=False)
+    name = Column(String(128), nullable=False)
