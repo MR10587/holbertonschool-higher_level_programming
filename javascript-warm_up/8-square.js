691bce30b7e48b94a5f5d095dea54bff169b53c5
MR10587 <ramalmirzezade20@gmail.com>
2026-02-01 16:39:29 +0000

    .

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-const sqr_size = process.argv[2];
+const sqrSize = process.argv[2];
 
-if (Number(sqr_size)) {
-  for (let i = 0; i < sqr_size; i++) {
-    console.log('X'.repeat(sqr_size));
+if (Number(sqrSize)) {
+  for (let i = 0; i < sqrSize; i++) {
+    console.log('X'.repeat(sqrSize));
   }
 } else {
   console.log('Missing size');
