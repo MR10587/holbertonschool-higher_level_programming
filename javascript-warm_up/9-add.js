3601544c7ecd2c87f473eff73964ff066759f005
MR10587 <ramalmirzezade20@gmail.com>
2026-02-01 16:46:26 +0000

    .

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
-function add(a, b) {
-    if (Number(process.argv[2]) && Number(process.argv[3])) {
-        const sum = Number(process.argv[2]) + Number(process.argv[3]);
-        console.log(sum);
-    } else {
-        console.log("NaN");
-    }
+function add (a, b) {
+  if (Number(process.argv[2]) && Number(process.argv[3])) {
+    const sum = Number(process.argv[2]) + Number(process.argv[3]);
+    console.log(sum);
+  } else {
+    console.log('NaN');
+  }
 }
 
-add()
+add();
