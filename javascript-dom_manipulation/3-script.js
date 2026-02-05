9e1ff070d7b06bcbb92da3674af55d36056721bb
MR10587 <ramalmirzezade20@gmail.com>
2026-02-05 18:57:40 +0000

    .

---
+++ javascript-dom_manipulation/3-script.js
@@ -0,0 +1,13 @@
+#!//usr/bin/node
+document.addEventListener("DOMContentLoaded", () => {
+    const header = document.querySelector("header")
+    const toggle = document.querySelector("#toggle_header")
+    
+    toggle.addEventListener("click", () =>{
+        if (header.classList.contains("red")) {
+            header.classList.replace("red", "green");
+        } else {
+            header.classList.replace("green", "red");
+        }
+    });
+});
