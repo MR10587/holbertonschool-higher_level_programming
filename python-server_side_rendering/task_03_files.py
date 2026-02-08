ee481db554fbeda43aa0c652ff16b91c58a34c0e
MR10587 <ramalmirzezade20@gmail.com>
2026-02-08 11:08:21 +0000

    .

---
+++ python-server_side_rendering/task_03_files.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+# Importing necessary modules and functions
+from flask import Flask, render_template, request
+import json
+import csv
+
+# Creating flask app
+app = Flask(__name__)
+
+# Route to products
+@app.route('/products')
+# Function to show in right route
+def products():
+    # Get source and ID
+    source = request.args.get('source')
+    id = request.args.get('id',type=int)
+
+    # Validate source
+    if source == 'json':
+        with open('products.json') as f:
+            products = json.load(f)
+    elif source == 'csv':
+        with open('products.csv') as f:
+            products = list(csv.DictReader(f))
+    else:
+        return "Wrong source", 400
+    
+    # Validate ID
+    if id:
+        if 1 <= id <= len(products):
+            products = [products[id - 1]]
+        else:
+            return "Product not found"
+
+    # Render HTML file
+    return render_template('product_display.html', products = products)
