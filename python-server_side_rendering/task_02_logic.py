16dbbf4c90a89a4a2e13f66a43ce7910e4ae9cc6
MR10587 <ramalmirzezade20@gmail.com>
2026-02-08 08:47:33 +0000

    .

---
+++ python-server_side_rendering/task_02_logic.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+from flask import Flask, render_template
+import json
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/items')
+def items():
+    items = []
+    with open('items.json', 'r') as f:
+            dicti = json.load(f)
+            for item in dicti.get("items"):
+                items.append(item)
+    print(items)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
