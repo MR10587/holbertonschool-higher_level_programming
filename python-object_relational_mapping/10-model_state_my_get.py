ce54ba7a3366ad7fceb6c26f050bf8cab5db67b3
MR10587 <ramalmirzezade20@gmail.com>
2026-02-13 16:12:10 +0400

    .

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -1,33 +1,36 @@
 #!/usr/bin/python3
-"""
-Prints the id of the first State matching the given name
-"""
-import sys
-from model_state import Base, State
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+"""Prints the id of the first State matching the given name."""
 
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from model_state import Base, State
 
 if __name__ == "__main__":
     if len(sys.argv) != 5:
-        print("Usage: ./10-model_state_my_get.py <mysql username> <mysql password> <database name> <state name searched>")
+        print(
+            "Usage: ./10-model_state_my_get.py "
+            "<mysql username> <mysql password> <database name> "
+            "<state name searched>"
+        )
         sys.exit(1)
 
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
-        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    engine = create_engine(
+        "mysql+mysqldb://%s:%s@localhost:3306/%s"
+        % (sys.argv[1], sys.argv[2], sys.argv[3])
+    )
 
-    state_which_was_given = sys.argv[4]
+    session = Session(engine)
+    state = (
+        session.query(State)
+        .filter(State.name == sys.argv[4])
+        .order_by(State.id)
+        .first()
+    )
 
-    Session = sessionmaker(bind=engine)
-    session = Session()
-
-    state = (session.query(State).
-             filter(State.name == state_which_was_given).
-             order_by(State.id).first())
-
-    if state:
-        print("{}".format(state.id))
+    if state is None:
+        print("Not found")
     else:
-        print('Not found')
+        print(state.id)
 
     session.close()
