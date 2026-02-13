2c8866fd2c2cd193b7387802a552c3eae60ebf23
MR10587 <ramalmirzezade20@gmail.com>
2026-02-13 16:14:15 +0400

    .

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -1,36 +1,21 @@
 #!/usr/bin/python3
-"""Prints the id of the first State matching the given name."""
+"""Model State"""
 
 import sys
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 from sqlalchemy.orm import Session
 from model_state import Base, State
 
 if __name__ == "__main__":
-    if len(sys.argv) != 5:
-        print(
-            "Usage: ./10-model_state_my_get.py "
-            "<mysql username> <mysql password> <database name> "
-            "<state name searched>"
-        )
-        sys.exit(1)
-
     engine = create_engine(
-        "mysql+mysqldb://%s:%s@localhost:3306/%s"
+        "mysql+mysqldb://%s:%s@localhost/%s"
         % (sys.argv[1], sys.argv[2], sys.argv[3])
     )
 
     session = Session(engine)
-    state = (
-        session.query(State)
-        .filter(State.name == sys.argv[4])
-        .order_by(State.id)
-        .first()
-    )
-
+    state = session.query(State).filter(State.name == sys.argv[4]).first()
     if state is None:
         print("Not found")
     else:
         print(state.id)
-
     session.close()
