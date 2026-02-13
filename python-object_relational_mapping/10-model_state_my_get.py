d0ee6caf71f431494913a8a5f7bb00b30c50fc00
MR10587 <ramalmirzezade20@gmail.com>
2026-02-13 16:10:07 +0400

    .

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -3,7 +3,7 @@
 Prints the id of the first State matching the given name
 """
 import sys
-from model_state import State
+from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
@@ -13,7 +13,7 @@ if __name__ == "__main__":
         print("Usage: ./10-model_state_my_get.py <mysql username> <mysql password> <database name> <state name searched>")
         sys.exit(1)
 
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
         sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
 
     state_which_was_given = sys.argv[4]
